ced87789d5e3473cc147260ac5b78fbae5835efe
mavismxy <e0758851@u.nus.edu>
2024-04-07 20:50:05 +0800

    trying out shiny

--- personal_app/app.R
+++ personal_app/app.R
@@ -1,24 +1,32 @@
 library(shiny)
 
+library(ggplot2)
+
 # Define UI for application that draws a histogram
 ui <- fluidPage(
   titlePanel("US Interest Rate"),
   sidebarLayout(
     sidebarPanel(
-      p("text on sidebar")
+      p("text on sidebar"),
+      sliderInput("year_data",
+                  label = "User Input: ",
+                  min = 1, 
+                  max = 98,
+                  value = 30)
     ),
     mainPanel(
-      p("text on main panel")
+      p("text on main panel"),
+      plotOutput("inflation_data")
     )
   )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-  output$distPlot <- renderPlot({
-    hist(rnorm(input$samplesize),col='darkorchid',xlab="Sample",main="Standard Normally Distributed Sample")},
-    height=300
-  )
+  output$inflation_data <- renderPlot({
+    ggplot(project_data) + geom_histogram(aes(x = inflation),
+                                          bins = input$year_data)
+  })
 }
 
 # Run the application 
