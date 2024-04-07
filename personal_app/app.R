c3f343532487c121eb06474c2cb02ecd8f4a7c16
mavismxy <e0758851@u.nus.edu>
2024-04-07 21:49:18 +0800

    colour of histogram

--- personal_app/app.R
+++ personal_app/app.R
@@ -12,7 +12,9 @@ ui <- fluidPage(
                   label = "User Input: ",
                   min = 1, 
                   max = 98,
-                  value = 30)
+                  value = 30),
+      selectInput("input1", "Choose colour: ",
+                  choices = c("red", "blue", "white"))
     ),
     mainPanel(
       p("text on main panel"),
@@ -25,7 +27,8 @@ ui <- fluidPage(
 server <- function(input, output) {
   output$inflation_data <- renderPlot({
     ggplot(project_data) + geom_histogram(aes(x = inflation),
-                                          bins = input$year_data)
+                                          bins = input$year_data,
+                                          fill = input$input1)
   })
 }
 
